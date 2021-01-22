3515aec08c4c3006d210fc1a6ac74b7d36431ed6
Belem Zhang <belem.zhang@intel.com>
2021-01-22 16:47:27 +0800

    i

--- src/components/header.js
+++ src/components/header.js
@@ -15,10 +15,10 @@ export default function Header() {
                 <div className="flex items-center justify-between w-full md:w-auto">
                   <button>
                     <span className="sr-only">Workflow</span>
-                    <img className="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="workflow" />
+                    <img className="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/workflow-mark-nn-600.svg" alt="workflow" />
                   </button>
                   <div className="-mr-2 flex items-center md:hidden">
-                    <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" id="main-menu" aria-haspopup="true">
+                    <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-nn-500" id="main-menu" aria-haspopup="true">
                       <span className="sr-only">Open main menu</span>
                       <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -31,7 +31,7 @@ export default function Header() {
                 <Link to="/getting-started/" className="font-medium text-gray-500 hover:text-gray-900">Getting Started</Link>
                 <Link to="/specs/" className="font-medium text-gray-500 hover:text-gray-900">Specs</Link>
                 <Link to="/community/" className="font-medium text-gray-500 hover:text-gray-900">Community</Link>
-                <Link to="/blog/mdx" className="font-medium text-indigo-600 hover:text-indigo-500">Blog</Link>
+                <Link to="/blog/mdx" className="font-medium text-nn-600 hover:text-nn-500">Blog</Link>
                 <Link to="/faq/" className="font-medium text-gray-500 hover:text-gray-900">FAQ</Link>
               </div>
             </nav>
@@ -40,10 +40,10 @@ export default function Header() {
             <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
               <div className="px-5 pt-4 flex items-center justify-between">
                 <div>
-                  <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="workflow" />
+                  <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-nn-600.svg" alt="workflow" />
                 </div>
                 <div className="-mr-2">
-                  <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <button type="button" className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-nn-500">
                     <span className="sr-only">Close main menu</span>
                     <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
@@ -58,7 +58,7 @@ export default function Header() {
                   <Link to="/community/" className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50" role="menuitem">Community</Link>
                 </div>
                 <div role="none">
-                  <Link to="/blog/mdx" className="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100" role="menuitem">
+                  <Link to="/blog/mdx" className="block w-full px-5 py-3 text-center font-medium text-nn bg-gray-50 hover:bg-gray-100" role="menuitem">
                     Blog
                   </Link>
                 </div>
@@ -71,7 +71,7 @@ export default function Header() {
             <div className="sm:text-center lg:text-left">
               <h1 className="text-2xl tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl">
                 <span className="block xl:inline">Accelerating </span>
-                <span className="block text-indigo-600 xl:inline">deep neural networks</span>
+                <span className="block text-nn-600 xl:inline">deep neural networks</span>
                 <span className="block xl:inline"> on the web</span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
@@ -79,12 +79,12 @@ export default function Header() {
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
-                  <button className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                  <button className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-nn-600 hover:bg-nn-700 md:py-4 md:text-lg md:px-10">
                     Getting Started
                   </button>
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <button className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10">
+                  <button className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-nn-700 bg-nn-100 hover:bg-nn-200 md:py-4 md:text-lg md:px-10">
                     Live Demos
                   </button>
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      blue: colors.blue,
+      red: colors.rose,
+      yellow: colors.amber,
+      nn: {
+        lightest: '#f9fafc',
+        light: '#ff7ce5',
+        DEFAULT: '#ff49db',
+        dark: '#ff16d1',
+        darkest: '#1f2d3d',
+        500: '#ff49db'
+      }
+    }
   },
   variants: {
     extend: {},
